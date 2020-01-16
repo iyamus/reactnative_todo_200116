ea68c4cd771e1c6597e7eebcebdd250738ac0ad1
iyamus <iyamus@gmail.com>
2020-01-16 14:28:45 -0700

    1_basic design

--- todo/App.js
+++ todo/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Simple To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New to do."}></TextInput>
+      </View>
     </View>
   );
 }
@@ -12,8 +18,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "300",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 30,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+
+    //플랫폼에 따라 속성이 달라지는 경우 platform 사용해 별도로 적용.
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        shawdowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+
+  }
 });
